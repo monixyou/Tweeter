22777316edd3054db8c1dd13007a4341b0e270be
Monica Bui <monicahb@fb.com>
2020-06-29 20:02:14 -0700

    Created data models for users and tweets

---
+++ twitter/Models/User.m
@@ -0,0 +1,24 @@
+//
+//  User.m
+//  twitter
+//
+//  Created by Monica Bui on 6/29/20.
+//  Copyright © 2020 Emerson Malca. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    self = [super init];
+    
+    if (self) {
+        self.name = dictionary[@"name"];
+        self.screenName = dictionary[@"screen_name"];
+    }
+    
+    return self;
+}
+
+@end
